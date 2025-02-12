80f5ac2cdf38d94f484bab43595e3d00f156057d
bangurad <bangurad@vcu.edu>
2025-02-11 20:20:51 -0500

    Create basic_js_syntax_DerickB.js

---
+++ basic_js_syntax_DerickB.js
@@ -0,0 +1,14 @@
+//striing vairiable to store my name
+String = "Rick";
+let name = "Derick B" ;
+//a number variable to represent my age
+let age = 19;
+//boolean variable indicating whether you are currently a student
+let isStudent = true
+//calculate your age in months using arthithmitic operations
+let ageInMonths = age * 12;
+//display result
+console.log("Age in months:", ageInMonths);
+
+
+
